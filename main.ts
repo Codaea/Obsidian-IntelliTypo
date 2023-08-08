d7d6cdce7191e9603f731c21e4ca3bc3b40dc76e
Codaea <93889672+Codaea@users.noreply.github.com>
2023-08-08 12:13:57 -0700

    Init Commit. Still Learning Typescript.

--- main.ts
+++ main.ts
@@ -2,68 +2,47 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface IntelliTypoSettings {
+	openWindow: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: IntelliTypoSettings = {
+	openWindow: 'Option + Enter'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class IntelliTypo extends Plugin {
+	settings: IntelliTypoSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		// Register a global keydown event listener for the hotkey
+        this.registerDomEvent(document, 'keydown', (evt: KeyboardEvent) => {
+            if (this.isHotkeyPressed(evt, this.settings.openWindow)) {
+                // The hotkey has been pressed, implement your behavior here
+                this.showIntelliTypoWindow();
+            }
+        });
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+
+		isHotkeyPressed(event: KeyboardEvent, hotkey: string): boolean {
+			const keys = hotkey.split('+');
+			const pressedKeys = keys.map((key) => key.trim().toLowerCase());
+	
+			for (const pressedKey of pressedKeys) {
+				if (!event[`${pressedKey}Key`]) {
+					return false;
 				}
 			}
-		});
+	
+			return true;
+		}
+
+
+
+
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
@@ -91,7 +70,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
+class TypoModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
@@ -108,9 +87,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: IntelliTypo;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: IntelliTypo) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,15 +99,21 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		// Add a hotkey setting
+        new Setting(containerEl)
+            .setName('Open IntelliTypo Window')
+            .setDesc('Set the hotkey to open the IntelliTypo window')
+            .addText((text) =>
+                text
+                    .setPlaceholder('Click to set')
+                    .setValue(this.plugin.settings.openWindow)
+                    .onChange(async (value) => {
+                        this.plugin.settings.openWindow = value;
+                        await this.plugin.saveSettings();
+                    })
+            );
+
+
 	}
 }
+function 
